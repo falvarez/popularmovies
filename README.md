2f2b553a7fafcea66e6994a442ececf8a8de1b76
Federico J. Álvarez Valero <fede.alvarez@gmail.com>
2017-02-07 00:42:27 +0100

    Add grid mock to main activity

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Popular Movies
 
 Udacity training project stage 1.
+
+## API calls
+
+Most popular: https://api.themoviedb.org/3/movie/popular?api_key=***REMOVED***
+
+Top rated: https://api.themoviedb.org/3/movie/top_rated?api_key=***REMOVED***
+
--- app/src/main/java/androidtraining/falvarez/es/popularmovies/MainActivity.java
+++ app/src/main/java/androidtraining/falvarez/es/popularmovies/MainActivity.java
@@ -2,6 +2,12 @@ package androidtraining.falvarez.es.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.movies_grid);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_most_popular) {
+            return true;
+        }
+
+        if (id == R.id.action_top_rated) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
