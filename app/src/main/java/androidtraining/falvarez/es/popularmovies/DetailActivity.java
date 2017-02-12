bdeff38f471ac2cbff983f34c8b4b42447b5af0f
Federico J. Álvarez Valero <fede.alvarez@gmail.com>
2017-02-12 01:01:33 +0100

    Refactor DetailActivity

--- app/src/main/java/androidtraining/falvarez/es/popularmovies/DetailActivity.java
+++ app/src/main/java/androidtraining/falvarez/es/popularmovies/DetailActivity.java
@@ -1,10 +1,8 @@
 package androidtraining.falvarez.es.popularmovies;
 
 import android.content.Intent;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,40 +18,28 @@ public class DetailActivity extends AppCompatActivity {
     private TextView mMovieLaunchDate;
     private TextView mMovieRating;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
-
+    private void configureViews() {
         mMovieTitle = (TextView) findViewById(R.id.detail_title_tv);
         mMovieDescription = (TextView) findViewById(R.id.detail_description_tv);
         mMoviePoster = (ImageView) findViewById(R.id.detail_poster_iv);
         mMovieLaunchDate = (TextView) findViewById(R.id.detail_launch_date_tv);
         mMovieRating = (TextView) findViewById(R.id.detail_rating_tv);
+    }
+
+    private void init() {
+        configureViews();
 
         Intent intentThatStartedThisActivity = getIntent();
 
         if (intentThatStartedThisActivity != null) {
-            if (intentThatStartedThisActivity.hasExtra("movieModel")) {
-                MovieModel movie = intentThatStartedThisActivity.getParcelableExtra("movieModel");
+            if (intentThatStartedThisActivity.hasExtra(MovieModel.EXTRA_MOVIE_MODEL)) {
+                MovieModel movie = intentThatStartedThisActivity.getParcelableExtra(MovieModel.EXTRA_MOVIE_MODEL);
                 showMovie(movie);
             }
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
     private void showMovie(MovieModel movie) {
-
         setTitle(movie.getTitle());
 
         String[] dateParts = movie.getLaunchDate().split("-");
@@ -61,12 +47,31 @@ public class DetailActivity extends AppCompatActivity {
         mMovieTitle.setText(movie.getTitle());
         mMovieDescription.setText(movie.getDescription());
         mMovieLaunchDate.setText(dateParts[0]);
-        mMovieRating.setText("User rating: " + movie.getRating());
+        mMovieRating.setText(String.format(getResources().getString(R.string.user_rating), movie.getRating()));
 
         RequestCreator requestCreator = Picasso
                 .with(this)
                 .load(movie.getPosterFullUrl(MovieModel.MEASURE_W342));
         requestCreator.fetch();
         requestCreator.into(mMoviePoster);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        init();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Mimic back button behaviour
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
